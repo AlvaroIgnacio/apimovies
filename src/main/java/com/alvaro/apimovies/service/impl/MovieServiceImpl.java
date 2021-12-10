29120e1c6b7f3e24eeb63b177101c2cbdd1cd5ac
AlvaroIgnacio <>
2021-12-10 18:41:21 -0300

    validacion para que no cree actores repetidos al crear una pelicula

--- src/main/java/com/alvaro/apimovies/service/impl/MovieServiceImpl.java
+++ src/main/java/com/alvaro/apimovies/service/impl/MovieServiceImpl.java
@@ -1,5 +1,6 @@
 package com.alvaro.apimovies.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.alvaro.apimovies.dto.MovieDTO;
 import com.alvaro.apimovies.dto.ReviewDTO;
+import com.alvaro.apimovies.model.Actor;
 import com.alvaro.apimovies.model.Movie;
 import com.alvaro.apimovies.projection.MovieProjection;
+import com.alvaro.apimovies.repository.ActorRepository;
 import com.alvaro.apimovies.repository.MovieRepository;
 import com.alvaro.apimovies.service.MovieService;
 import com.alvaro.apimovies.service.ReviewService;
@@ -20,11 +23,14 @@ public class MovieServiceImpl implements MovieService {
 
     private final ObjectMapper mapper;
     private final MovieRepository movieRepository;
+    private final ActorRepository actorRepository;
     private final ReviewService reviewService;
-
+    //private final Logger log = LoggerFactory.getLogger(getClass()); 
+    
     @Autowired
-    MovieServiceImpl(MovieRepository movieRepo, ObjectMapper mapper, ReviewService reviewService) {
+    MovieServiceImpl(MovieRepository movieRepo, ObjectMapper mapper, ReviewService reviewService, ActorRepository actorRepository) {
         this.movieRepository = movieRepo;
+        this.actorRepository = actorRepository;
         this.mapper = mapper;
         this.reviewService = reviewService;
     }
@@ -32,7 +38,21 @@ public class MovieServiceImpl implements MovieService {
     @Override
     @Transactional
     public Movie createMovie(MovieDTO movieDTO) {
+    	if (movieRepository.findProjectedByName(movieDTO.getName()) != null) {
+    		throw new RuntimeException("Ya existe una película con el nombre ingresado");
+    	}
         Movie movie = mapper.convertValue(movieDTO, Movie.class);
+        List<Actor> actores = new ArrayList<>();
+        List<Actor> candidatos = new ArrayList<>();
+        for (Actor actor: movie.getActors()) {
+        	candidatos = actorRepository.findByLastNameIgnoreCaseAndNameIgnoreCase(actor.getLastName(), actor.getName());
+        	if (candidatos.size()>0) {
+        		actores.add(candidatos.get(0));
+        	} else {
+        		actores.add(actor);
+        	}
+        }
+        movie.setActors(actores);
         return movieRepository.save(movie);
     }
 
